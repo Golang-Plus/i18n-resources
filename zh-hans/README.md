962c9cee8c33462aad3f8769750ad27fa5d478dc
wayn3h0 <hi@wayn3h0.com>
2017-03-22 07:22:36 +0800

    add zh-Hans resources

---
+++ zh-hans/README.md
@@ -0,0 +1,11 @@
+# Resources [zh-Hans]
+
+This package providers **zh-Hans** resources for [i18n](https://github.com/golang-plus/i18n) package.
+
+
+## Usage
+
+
+    import (
+        _ "github.com/golang-plus/i18n-resources/zh-Hans"
+    )
---
+++ zh-hans/zh-hans.go
@@ -0,0 +1,38 @@
+package zh_hans
+
+import (
+	"github.com/golang-plus/i18n"
+)
+
+// load resources.
+func init() {
+	lang := i18n.GetLanguage("zh-Hans")
+
+	for code, name := range languageNames {
+		language := i18n.GetLanguage(code)
+		//if language != nil {  // we need panic if data wrong
+		language.Name.SetValue(lang, name)
+		//}
+	}
+
+	for code, name := range countryNames {
+		country := i18n.GetCountry(nil, code)
+		//if country != nil {  // we need panic if data wrong
+		country.Name.SetValue(lang, name)
+		//}
+	}
+
+	for code, name := range currencyNames {
+		currency := i18n.GetCurrency(code)
+		//if currency != nil {  // we need panic if data wrong
+		currency.Name.SetValue(lang, name)
+		//}
+	}
+
+	for code, name := range cultureNames {
+		culture := i18n.GetCulture(code)
+		//if cculture != nil {  // we need panic if data wrong
+		culture.Name.SetValue(lang, name)
+		//}
+	}
+}
